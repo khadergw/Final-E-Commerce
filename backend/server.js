a4320e8661172316110c7f476925e78de861a19f
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2022-02-21 00:41:07 +0400

    Create Signin Backend

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,38 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from "./data.js";
+// import data from "./data.js";
 import userRouter from "./routers/userRouter.js";
+<<<<<<< HEAD
 import dotenv from 'dotenv';
 
 dotenv.config();
+=======
+import productRouter from './routers/productRouter.js';
+>>>>>>> c37c5875077006a037e97e60c059f247afd7a5c8
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 /*to get the details of a product */
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => {
-    return Number(x._id) === Number(req.params.id);
-  });
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+// app.get("/api/products/:id", (req, res) => {
+//   const product = data.products.find((x) => {
+//     return Number(x._id) === Number(req.params.id);
+//   });
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: "Product Not Found" });
+//   }
+// });
 app.use("/api/users", userRouter);
+app.use('/api/products', productRouter);
 app.get("/", (req, res) => {
   res.send("Server is Listening!");
 });
 /*to get the list of the products */
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+// app.get("/api/products", (req, res) => {
+//   res.send(data.products);
+// });
 /*display error */
 app.use((err, req, res, next) => {
   res.status(500).send({message: err.message});
